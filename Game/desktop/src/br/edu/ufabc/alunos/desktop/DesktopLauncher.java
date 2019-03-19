124c1b2f4ca2d1f9083142291fdd07eb462e9464
Andre Barbosa <an.barbosa@outlook.com.br>
2019-03-19 19:37:35 -0300

    Projeto criado usando o LibGDX.

---
+++ Game/desktop/src/br/edu/ufabc/alunos/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.edu.ufabc.alunos.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.edu.ufabc.alunos.Unnamed;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Unnamed(), config);
+	}
+}
